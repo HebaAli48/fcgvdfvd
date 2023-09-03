9c16272bf514d84693128016c2ef8858922cd9a2
HebaAli48 <131808003+HebaAli48@users.noreply.github.com>
2023-09-04 01:31:42 +0300

    routed more project to github profile

--- src/components/SeeMore.tsx
+++ src/components/SeeMore.tsx
@@ -1,15 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Forword } from "../utils/Icons";
-
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
 const SeeMore: React.FC = () => {
+  const [refSeeMore, inViewSeeMore] = useInView({
+    threshold: 0.5,
+  });
   return (
-    <Link
-      to="/"
-      className="mb-2 flex gap-1 items-center text-lg text-btn-primary hover:translate-x-[2px] hover:text-sky-400 transition-all"
+    <motion.div
+      ref={refSeeMore}
+      initial={{ opacity: 0, x: -50 }} // Initial position and opacity for h2 (move from the left and be hidden).
+      animate={inViewSeeMore ? { opacity: 1, x: 0 } : { opacity: 0, x: -50 }} // Animate when in view.
+      exit={{ opacity: 0, x: -50 }} // Animate when exiting view.
+      transition={{ duration: 1 }}
     >
-      <span className="ml-auto">More Projects </span> <span>{Forword}</span>
-    </Link>
+      <Link
+        to="https://github.com/HebaAli48?tab=repositories"
+        className="mb-2 flex gap-1 items-center text-lg text-btn-primary hover:translate-x-[2px] hover:text-sky-400 transition-all"
+      >
+        <span className="ml-auto">More Projects </span> <span>{Forword}</span>
+      </Link>
+    </motion.div>
   );
 };
 
